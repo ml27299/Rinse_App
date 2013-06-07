33be0548fd152f38ef09bffd268902973f6e64f8
Mac <mac@salsamobi.com>
2013-06-07 11:18:55 -0500

    UI Fixes

--- Rinse-PG/www/js/signup.js
+++ Rinse-PG/www/js/signup.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-var base_url = 'http://rinse.herokuapp.com/public/user';
+var base_url = 'http://rinse.herokuapp.com/public';
 
 function goBack(){
     history.go(-1);
@@ -27,21 +27,21 @@ function goNext(file){
     
     if(file == 'signup2.html'){
         var name =  document.getElementById('name').value;
-        window.localStorage.setItem("name", name);
-        var email =document.getElementById('email').value;
-        window.localStorage.setItem("email", email);
-        var phone = document.getElementById('phone').value;
-        window.localStorage.setItem("phone", document.getElementById('phone').value);
         var password =document.getElementById('password').value;
+        var email =document.getElementById('email').value;
+        var phone = document.getElementById('phone').value;
+        window.localStorage.setItem("name", name);
+        window.localStorage.setItem("email", email);
+        window.localStorage.setItem("phone", phone);
         window.localStorage.setItem("password", password);
     }else if(file == 'credit_card.html'){
         var address = document.getElementById('address').value;
         var address2 = document.getElementById('address2').value;
         var zip = document.getElementById('zip').value;
         window.localStorage.setItem("address", address);
+        window.localStorage.setItem("zip", zip);
         if(address2)
             window.localStorage.setItem("address2", address2);
-        window.localStorage.setItem("zip", zip);
     }
     
     if((name && email && phone && password) || (address && zip))
@@ -52,15 +52,11 @@ function goNext(file){
                             reloadPage:true
                             });
     else
-        navigator.notification.alert(
-                                     'Please Fill out all fields',  // message
-                                     alertDismissed,         // callback
-                                     'Empty Field',            // title
-                                     'OK'                  // buttonName
-                                     );
+        goAlert();
 }
+
 function alertDismissed() {
-    // do something
+    // do nothing
 }
 
 function post(){
@@ -75,33 +71,29 @@ function post(){
     if(address && zip){
         $.ajax({
            //type: "POST",
-           url: base_url+"/post",
-           dataType: "json",
-           contentType: "application/json",
-           data: {
-           "name":name,
-           "email":email,
-           "phone":phone,
-           "password":password,
-           "address":address,
-           "address2":address2,
-           "zip":zip
-           },
-           success: function(result) {
-           alert("Success: " + JSON.stringify(result));
-           },
-           error: function() {
-           alert("Error: " + JSON.stringify(arguments));
-           }
+                url: base_url+"/user/post",
+                dataType: "json",
+                contentType: "application/json",
+                data: {
+                        "name":name,
+                        "email":email,
+                        "phone":phone,
+                        "password":password,
+                        "address":address,
+                        "address2":address2,
+                        "zip":zip
+                },
+               success: function(result) {
+                    alert("Success: " + JSON.stringify(result));
+                    
+               },
+                error: function(arguments) {
+                    alert("Error: " + JSON.stringify(arguments));
+               }
            });
     goNext('credit_card.html');
     }else
-        navigator.notification.alert(
-                                     'Please Fill out all fields',  // message
-                                     alertDismissed,         // callback
-                                     'Empty Field',            // title
-                                     'OK'                  // buttonName
-                                     );
+        goAlert();
     
 }
 
@@ -112,4 +104,46 @@ function hasAccount(){
                         changeHash:true,
                         reloadPage:true
                         });
+}
+
+function signIn(){
+    
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    if(email && password){
+        $.ajax({
+               //type: "POST",
+               url: base_url+"/user/get",
+               dataType: "json",
+               contentType: "application/json",
+               data: {
+               "email":email,
+               "password":password
+               },
+               success: function(result) {
+                    alert("Success: " + JSON.stringify(result));
+                    var objJSON = eval("(function(){return " + JSON.stringify(result) + ";})()");
+                    var user_info = new Array();
+                
+               
+               },
+               error: function(arguments) {
+               alert("Error: " + JSON.stringify(arguments));
+               }
+               });
+        goNext('credit_card.html');
+    }else
+        goAlert();
+        
+    
+}
+
+function goAlert(){
+    navigator.notification.alert(
+                                 'Please Fill out all fields',  // message
+                                 alertDismissed,         // callback
+                                 'Empty Field',            // title
+                                 'OK'                  // buttonName
+                                 );
 }
