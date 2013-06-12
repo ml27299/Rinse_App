c121bc2148b5b633cf986be9a68a573982a03459
Mac <mac@salsamobi.com>
2013-06-12 15:10:48 -0500

    Logout Functionality

--- Rinse-PG/www/js/index.js
+++ Rinse-PG/www/js/index.js
@@ -18,7 +18,7 @@
  */
 
 
-
+var main_script = 'js/main.js';
 
 function onDeviceReady() {
     navigator.splashscreen.hide();
@@ -35,4 +35,32 @@ function nextPage(){
         $.getScript(main_script, function(){movePage('pages/signup.html','slidefade','page',true,true);});
     else
         $.getScript(main_script, function(){movePage('pages/confirm_now.html','slidefade','page',true,true);});
+}
+
+function logOut(){
+    refreshPage();
+    var name = window.localStorage.getItem('name');
+    var email = window.localStorage.getItem('email');
+    var phone = window.localStorage.getItem('phone');
+    var address = window.localStorage.getItem('address');
+    var zip = window.localStorage.getItem('zip');
+
+    if(email && name && phone && zip && address)
+        document.write('<a href="javascript:goLogOut();" align = "center">Log Out?</a>');
+}
+
+function goLogOut(){
+    var main_script = 'js/main.js';
+    window.localStorage.clear();
+    $.getScript(main_script, function(){goAlert('You have been logged out','Logout');});
+    refreshPage();
+}
+
+function refreshPage()
+{
+    $.mobile.changePage(window.location.href, {
+                             allowSamePageTransition: true,
+                             transition: 'none',
+                             reloadPage: true
+                             });
 }
